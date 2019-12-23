58f155badd9b826f2d5831cacec56344f339a584
Joy <joycok@gmail.com>
2019-12-23 09:32:34 +0530

    save

--- src/app/clearing/orderlist/store/orderlist.actions.ts
+++ src/app/clearing/orderlist/store/orderlist.actions.ts
@@ -5,4 +5,3 @@ import { createAction , props } from '@ngrx/store'
 
 export const RequestLoad = createAction('[ORDER LIST] LOAD REQUEST', props<{searchQuery : SearchQuery, pageQuery : PageQuery}>() );
 export const RequestLoadCompleted  = createAction('[ORDER LIST] LOAD REQUEST COMPLETE', props<{data : JobOrderModel}>());
-
