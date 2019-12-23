7d53d0d9c453f7c8f2c723ada5bfb32b03150611
Joy <joycok@gmail.com>
2019-12-23 15:50:11 +0530

    save

--- src/app/clearing/clearing.module.ts
+++ src/app/clearing/clearing.module.ts
@@ -1,25 +1,29 @@
 import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+
 import { SharedModule } from '../shared/shared.module';
 import { ClearingRoutingModule } from './clearing.routing.module';
+
 import { ClearingComponent } from './clearing.component';
-
-
 import { JobOrderComponent } from './order/joborder.component';
-
 import { JobContainerComponent } from './container/jobcontainer.component';
-
 import { OrderListComponent } from './orderlist/orderlist.component';
-
 import { AgentBookComponent } from './agentbook/agentbook.component';
 import { TrackOrderComponent } from './trackorder/trackorder.component';
 import { WeekPlanningComponent } from './weekplanning/weekplanning.component';
 import {EdiOrderComponent } from './ediorder/ediorder.component';
 import {EdiOrdUpdateComponent} from './ediordupdate/ediordupdate.component';
 
+import { OrderListReducer } from './orderlist/store/orderlist.reducer';
+import { OrderListEffects } from './orderlist/store/orderlist.effects';
+
 @NgModule({
   imports: [
     SharedModule,
-    ClearingRoutingModule
+    ClearingRoutingModule,
+    StoreModule.forFeature('orderlist', OrderListReducer),
+    EffectsModule.forFeature([ OrderListEffects])
   ],
   declarations: [
     ClearingComponent,
--- src/app/clearing/orderlist/store/orderlist.reducer.ts
+++ src/app/clearing/orderlist/store/orderlist.reducer.ts
@@ -1,26 +1,93 @@
 import { EntityState, EntityAdapter, createEntityAdapter } from '@ngrx/entity';
-import { Action, createReducer, on } from '@ngrx/store';
-import { Joborderm, JobOrderModel } from '../../models/joborder';
+import { Action, createReducer, on, createFeatureSelector, createSelector } from '@ngrx/store';
+import { Joborderm, JobOrderModel, SearchQuery, PageQuery } from '../../models/joborder';
 import * as AllActions from './orderlist.actions';
- 
-export interface State extends EntityState<JobOrderModel> {
+import { SelectRouterUrlId } from 'src/app/reducers';
 
-} 
-export const adapter: EntityAdapter<JobOrderModel> = createEntityAdapter<JobOrderModel>({
-    selectId : order => order.urlid
-});
- 
-export const initialState: State = adapter.getInitialState({
-});
- 
-export const _Reducer = createReducer(
+export interface OrderListState extends EntityState<JobOrderModel> {
+
+}
+
+export const adapter: EntityAdapter<JobOrderModel> = createEntityAdapter<JobOrderModel>({ selectId: orderModel => orderModel.urlid });
+
+export const initialState: OrderListState = adapter.getInitialState();
+
+export const Reducer = createReducer(
   initialState,
-  on(AllActions.RequestLoadCompleted, (state,action) => {
+  on(AllActions.RequestLoadCompleted, (state, action) => {
     adapter.removeOne(action.data.urlid, state);
-    return adapter.addOne( action.data, state );
+    return adapter.addOne(action.data, state);
   })
 );
- 
-export function reducer(state: State | undefined, action: Action) {
-  return _Reducer(state, action);
+
+export function OrderListReducer(state: OrderListState | undefined, action: Action) {
+  return Reducer(state, action);
 }
+
+export const { selectAll, selectEntities, selectIds, selectTotal } = adapter.getSelectors();
+
+export const SelectJobOrderState = createFeatureSelector<OrderListState>('orderlist');
+
+export const SelectEntity = createSelector(
+  SelectJobOrderState,
+  SelectRouterUrlId,
+  (state: OrderListState, urlid) => {
+    if (state.entities[urlid])
+      return state.entities[urlid];
+    else
+      return null;
+  }
+);
+
+export const SelectSearchRecord = createSelector(
+  SelectEntity,
+  (record: JobOrderModel) => {
+    if (record)
+      return record.searchQuery;
+    else
+      return <SearchQuery>{};
+  }
+);
+
+export const SelectPageQuery = createSelector(
+  SelectEntity,
+  (record: JobOrderModel) => {
+    if (record)
+      return record.pageQuery;
+    else
+      return <PageQuery>{action :'NEW',page_count :0, page_current :0, page_rowcount :0, page_rows :50};
+  }
+);
+
+export const SelectRecords = createSelector(
+  SelectEntity,
+  (record: JobOrderModel) => {
+    if (record)
+      return record.records;
+    else
+      return null;
+  }
+);
+
+
+export const SelectMessage = createSelector(
+  SelectEntity,
+  (record: JobOrderModel) => {
+    if (record)
+      return record.message;
+    else
+      return null;
+  }
+);
+
+export const SelectIsError = createSelector(
+  SelectEntity,
+  (record: JobOrderModel) => {
+    if (record)
+      return record.isError;
+    else
+      return null;
+  }
+);
+
+
