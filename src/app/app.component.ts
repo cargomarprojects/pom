e5d3bc77978080a4f98c09ac0a3e5a1d3b4577c4
joy <joycok@gmail.com>
2020-01-15 17:35:48 +0530

    save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit} from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Event, NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router } from '@angular/router';
 import { GlobalService } from './core/services/global.service';
 
@@ -10,13 +10,13 @@ export class AppComponent implements OnDestroy {
     title = 'Application Root Page';
     loading = false;
 
-    sub : any;
+    sub: any;
 
     constructor(
-        private gs : GlobalService,
+        private gs: GlobalService,
         private router: Router
     ) {
-        this.sub =  this.router.events.subscribe((event: Event) => {
+        this.sub = this.router.events.subscribe((event: Event) => {
             switch (true) {
                 case event instanceof NavigationStart: {
                     this.loading = true;
@@ -36,21 +36,24 @@ export class AppComponent implements OnDestroy {
     }
 
 
-    ngOnInit(){
+    ngOnInit() {
         if (localStorage.length > 0) {
-            this.gs.MenuList = JSON.parse(localStorage.getItem('menu'));
-            this.gs.Modules = JSON.parse(localStorage.getItem('modules'));
-            this.gs.globalVariables = JSON.parse(localStorage.getItem('gv'));
-            this.gs.defaultValues = JSON.parse(localStorage.getItem('dv'));
-            this.gs.Access_Token = localStorage.getItem('access_token');
-            if ( this.gs.Access_Token) {
-                this.gs.IsAuthenticated= true ;
-                this.gs.IsLoginSuccess =true;
+
+            if (localStorage.getItem('access_token')) {
+                this.gs.MenuList = JSON.parse(localStorage.getItem('menu'));
+                this.gs.Modules = JSON.parse(localStorage.getItem('modules'));
+                this.gs.globalVariables = JSON.parse(localStorage.getItem('gv'));
+                this.gs.defaultValues = JSON.parse(localStorage.getItem('dv'));
+                this.gs.Access_Token = localStorage.getItem('access_token');
+                if (this.gs.Access_Token) {
+                    this.gs.IsAuthenticated = true;
+                    this.gs.IsLoginSuccess = true;
+                }
             }
         }
     }
 
-    ngOnDestroy(){
+    ngOnDestroy() {
         this.sub.unsubscribe();
     }
 
