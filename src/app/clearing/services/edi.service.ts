d4528744a47002589986200af7ab28a7cc780590
joy <joycok@gmail.com>
2020-02-03 10:09:35 +0530

    errlst

--- src/app/clearing/services/edi.service.ts
+++ src/app/clearing/services/edi.service.ts
@@ -6,41 +6,12 @@ import { GlobalService } from '../../core/services/global.service';
 @Injectable({providedIn:'root'})
 export class EdiService {
 
-  partnerid = '';
-  ErrorMessage = "";
-  InfoMessage = "";
   EdiErrorList: [] = [];
 
   constructor(
     private http2: HttpClient,
     private gs: GlobalService) {
   }
-
-
-
-  Validate(_type: string) {
-
-    let SearchData = {
-      company_code: this.gs.globalVariables.comp_code,
-      user_code: this.gs.globalVariables.user_code,      
-      partnerid: this.partnerid,
-    };
-
-    this.ErrorMessage = '';
-    this.InfoMessage = '';
-    this.getValidate(SearchData)
-      .subscribe(response => {
-        this.EdiErrorList = response.list;
-        if ( response.list.length >0 )
-          alert('pls check the Error List tab to see the Missing Data');
-        else
-        alert('No Missing Data Found');
-      },
-        error => {
-          this.ErrorMessage = this.gs.getError(error);
-        });
-  }
-
 
   List(SearchData: any) {
     return this.http2.post<any>(this.gs.baseUrl + '/api/Operations/JobOrderEdi/List', SearchData, this.gs.headerparam2('authorized'));
@@ -53,10 +24,6 @@ export class EdiService {
 
   ImportEdiFiles(SearchData: any) {
     return this.http2.post<any>(this.gs.baseUrl + '/api/edi/ImportEdiFiles', SearchData, this.gs.headerparam2('authorized'));
-  }
-
-  getValidate(SearchData: any) {
-    return this.http2.post<any>(this.gs.baseUrl + '/api/Edi/Order/Validate', SearchData, this.gs.headerparam2('authorized'));
   }
   
   UpdateOrdersList(SearchData: any) {
