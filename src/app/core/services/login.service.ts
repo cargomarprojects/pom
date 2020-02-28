6fb3cac5b1c42d06881f93e2612398971f5bcc04
joy <joycok@gmail.com>
2020-02-28 10:21:13 +0530

    hs

--- src/app/core/services/login.service.ts
+++ src/app/core/services/login.service.ts
@@ -5,45 +5,55 @@ import { HttpClient } from '@angular/common/http';
 //import 'rxjs/add/operator/map';
 
 import { GlobalService } from './global.service';
+import { HouseListService } from '../../Clearing/services/houselist.service';
 
 @Injectable()
 export class LoginService {
-    constructor(
-        private http2: HttpClient,
-        private gs: GlobalService) {
-    }
+  constructor(
+    private http2: HttpClient,
+    private gs: GlobalService,
+    private hs: HouseListService
+  ) {
+  }
 
-  
-    Login(username: string, password: string, company_code : string ) {
-        var body = 'grant_type=' + 'password' + '&username=' + username + '&password=' + password ;
-        return this.http2.post<any>(this.gs.baseUrl + "/Token", body, this.gs.headerparam2('login', company_code));
-    }
 
-    Logout() {
-        this.gs.IsLoginSuccess = false;
-        this.gs.IsAuthenticated = false;
-        this.gs.Access_Token = '';
-    }
+  Login(username: string, password: string, company_code: string) {
 
-    LoadMenu(SearchData : any) {
-      return this.http2.post<any>(this.gs.baseUrl + "/api/Admin/User/LoadMenu", SearchData, this.gs.headerparam2('authorized'));
-    }
+    this.initAllService();
 
-    LoadBranch(SearchData: any) {
-      return this.http2.post<any>(this.gs.baseUrl + "/api/Admin/User/LoadBranch", SearchData, this.gs.headerparam2('authorized'));
-    }
+    var body = 'grant_type=' + 'password' + '&username=' + username + '&password=' + password;
+    return this.http2.post<any>(this.gs.baseUrl + "/Token", body, this.gs.headerparam2('login', company_code));
+  }
 
-    LoadCompany(SearchData: any) {
-      return this.http2.post<any>(this.gs.baseUrl + "/api/Admin/User/LoadCompany", SearchData, this.gs.headerparam2('anonymous'));
-    }
+  initAllService() {
+    this.hs.init();
+  }
 
-    LoadYear(SearchData: any) {
-      return this.http2.post<any>(this.gs.baseUrl + "/api/Admin/User/LoadYear", SearchData, this.gs.headerparam2('authorized'));
-    }
+  Logout() {
+    this.gs.IsLoginSuccess = false;
+    this.gs.IsAuthenticated = false;
+    this.gs.Access_Token = '';
+  }
 
-    CheckLocalServer(SearchData: any) {
-      return this.http2.post<any>(this.gs.baseLocalServerUrl + "/api/values/GetVersion", SearchData, this.gs.headerparam2('anonymous'));
-    }
+  LoadMenu(SearchData: any) {
+    return this.http2.post<any>(this.gs.baseUrl + "/api/Admin/User/LoadMenu", SearchData, this.gs.headerparam2('authorized'));
+  }
+
+  LoadBranch(SearchData: any) {
+    return this.http2.post<any>(this.gs.baseUrl + "/api/Admin/User/LoadBranch", SearchData, this.gs.headerparam2('authorized'));
+  }
+
+  LoadCompany(SearchData: any) {
+    return this.http2.post<any>(this.gs.baseUrl + "/api/Admin/User/LoadCompany", SearchData, this.gs.headerparam2('anonymous'));
+  }
+
+  LoadYear(SearchData: any) {
+    return this.http2.post<any>(this.gs.baseUrl + "/api/Admin/User/LoadYear", SearchData, this.gs.headerparam2('authorized'));
+  }
+
+  CheckLocalServer(SearchData: any) {
+    return this.http2.post<any>(this.gs.baseLocalServerUrl + "/api/values/GetVersion", SearchData, this.gs.headerparam2('anonymous'));
+  }
 
 
 
