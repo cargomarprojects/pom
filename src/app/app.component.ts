89f8207f0b193e6454de4b3659f97e4eeefa8490
Joy <joycok@gmail.com>
2020-03-03 11:46:06 +0530

    save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Event, NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router } from '@angular/router';
 import { GlobalService } from './core/services/global.service';
+import { environment } from 'src/environments/environment';
+
+
+
 
 @Component({
     selector: 'app-root',
@@ -37,25 +41,28 @@ export class AppComponent implements OnDestroy {
 
 
     ngOnInit() {
-        if (localStorage.length > 0) {
 
-            if (localStorage.getItem('access_token')) {
-                this.gs.MenuList = JSON.parse(localStorage.getItem('menu'));
-                this.gs.Modules = JSON.parse(localStorage.getItem('modules'));
-                this.gs.globalVariables = JSON.parse(localStorage.getItem('gv'));
-                this.gs.defaultValues = JSON.parse(localStorage.getItem('dv'));
-                this.gs.TradingPartners = JSON.parse(localStorage.getItem('tp'));
-                this.gs.Access_Token = localStorage.getItem('access_token');
-                this.gs.Company_Name = localStorage.getItem('company_name');
-                if (this.gs.Access_Token) {
-                    this.gs.IsAuthenticated = true;
-                    this.gs.IsLoginSuccess = true;
-
-                 
+        if (environment.production) {
+            this.router.navigate(['login'], { replaceUrl: true });            
+        }
+        else {
+            if (localStorage.length > 0) {
+                if (localStorage.getItem('access_token')) {
+                    this.gs.MenuList = JSON.parse(localStorage.getItem('menu'));
+                    this.gs.Modules = JSON.parse(localStorage.getItem('modules'));
+                    this.gs.globalVariables = JSON.parse(localStorage.getItem('gv'));
+                    this.gs.defaultValues = JSON.parse(localStorage.getItem('dv'));
+                    this.gs.TradingPartners = JSON.parse(localStorage.getItem('tp'));
+                    this.gs.Access_Token = localStorage.getItem('access_token');
+                    this.gs.Company_Name = localStorage.getItem('company_name');
+                    if (this.gs.Access_Token) {
+                        this.gs.IsAuthenticated = true;
+                        this.gs.IsLoginSuccess = true;
+                    }
                 }
             }
         }
-        
+
     }
 
     ngOnDestroy() {
