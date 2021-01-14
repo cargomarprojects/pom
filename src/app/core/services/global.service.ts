461acad01419e48a7ea17dccbab6daae84c6128e
Joy <joycok@gmail.com>
2021-01-14 16:23:19 +0530

    save

--- src/app/core/services/global.service.ts
+++ src/app/core/services/global.service.ts
@@ -20,9 +20,9 @@ export class GlobalService {
   public defaultValues: DefaultValues;
 
   public baseLocalServerUrl: string = "http://localhost:8080";
-  public baseUrl: string = "http://localhost:5000";
+  //public baseUrl: string = "http://localhost:5000";
   //public baseUrl: string = "https://localhost:44340";
-  //public baseUrl: string = "";
+  public baseUrl: string = "";
 
 
 
