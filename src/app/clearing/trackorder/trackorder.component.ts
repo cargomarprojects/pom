40788537c5537cc8aa795b04a1b033d3f5777d53
joy <joycok@gmail.com>
2023-06-02 09:38:24 +0530

    trk

--- src/app/clearing/trackorder/trackorder.component.ts
+++ src/app/clearing/trackorder/trackorder.component.ts
@@ -81,30 +81,29 @@ export class TrackOrderComponent {
 
   // Init Will be called After executing Constructor
   ngOnInit() {
-    // this.LoadCaptions();
-
+     this.LoadCaptions();
   }
 
   LoadCaptions() {
-    this.loading = true;
-    let SearchData = {
-      comp_code: this.gs.globalVariables.comp_code,
-      branch_code: this.gs.globalVariables.branch_code,
-      user_code: this.gs.globalVariables.user_code
-    };
-
-    this.ErrorMessage = '';
-    this.InfoMessage = '';
-    this.mainService.GetTrackingCaptions(SearchData)
-      .subscribe(response => {
-        this.loading = false;
-        this.TrkCaptionList = response.list;
-      },
-        error => {
-          this.loading = false;
-          this.ErrorMessage = this.gs.getError(error);
-        });
-
+    /* this.loading = true;
+     let SearchData = {
+       comp_code: this.gs.globalVariables.comp_code,
+       branch_code: this.gs.globalVariables.branch_code,
+       user_code: this.gs.globalVariables.user_code
+     };
+ 
+     this.ErrorMessage = '';
+     this.InfoMessage = '';
+     this.mainService.GetTrackingCaptions(SearchData)
+       .subscribe(response => {
+         this.loading = false;
+         this.TrkCaptionList = response.list;
+       },
+         error => {
+           this.loading = false;
+           this.ErrorMessage = this.gs.getError(error);
+         });
+ */
   }
 
   process() {
@@ -131,16 +130,6 @@ export class TrackOrderComponent {
     this.Record.ord_track_status = '';
     this.Record.ord_dlv_pol_date = '';
     this.Record.ord_dlv_pod_date = '';
-    this.Record.ord_booking_date_enabled = true;
-    this.Record.ord_rnd_insp_date_enabled = true;
-    this.Record.ord_po_rel_date_enabled = true;
-    this.Record.ord_cargo_ready_date_enabled = true;
-    this.Record.ord_fcr_date_enabled = true;
-    this.Record.ord_insp_date_enabled = true;
-    this.Record.ord_stuf_date_enabled = true;
-    this.Record.ord_whd_date_enabled = true;
-    this.Record.ord_dlv_pol_date_enabled = true;
-    this.Record.ord_dlv_pod_date_enabled = true;
   }
 
 
@@ -187,33 +176,6 @@ export class TrackOrderComponent {
     this.Record.rec_mode = this.mode;
   }
 
-  // Save Data
-  // Update() {
-
-  //   this.ErrorMessage = '';
-  //   this.InfoMessage = '';
-  //   if (this.Record.ord_pkid.trim().length <= 0) {
-  //     this.ErrorMessage = " Cannot Save Invalid ID";
-  //     alert(this.ErrorMessage);
-  //     return;
-  //   }
-
-  //   this.loading = true;
-  //   this.ErrorMessage = '';
-  //   this.InfoMessage = '';
-  //   this.Record._globalvariables = this.gs.globalVariables;
-  //   this.mainService.Update(this.Record)
-  //     .subscribe(response => {
-  //       this.loading = false;
-  //       // this.InfoMessage = "Save Complete";
-  //       this.Close();
-  //     },
-  //       error => {
-  //         this.loading = false;
-  //         this.ErrorMessage = this.gs.getError(error);
-  //         alert(this.ErrorMessage);
-  //       });
-  // }
 
   // Save Data
   Update(_type: string) {
@@ -235,7 +197,6 @@ export class TrackOrderComponent {
       .subscribe(response => {
         this.loading = false;
         // this.InfoMessage = "Save Complete";
-        // this.EnableSave(response.enableseq);
         this.Close();
       },
         error => {
@@ -329,6 +290,6 @@ export class TrackOrderComponent {
       return _type;
   }
   EnableLastdate() {
-    this.enableDateCode =this.lastDateCode;
+    this.enableDateCode = this.lastDateCode;
   }
 }
