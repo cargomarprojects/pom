a6675a1a1a481aeec989141ad574162c351d987a
joy <cargomarprojects@outlook.com>
2024-10-23 12:14:43 +0530

    save

--- src/app/clearing/mblm/edit/other/cntr/cntr.component.ts
+++ src/app/clearing/mblm/edit/other/cntr/cntr.component.ts
@@ -7,304 +7,308 @@ import { CntrService } from '../../../../services/cntr.service';
 import { SearchTable } from '../../../../../shared/models/searchtable';
 
 @Component({
-    selector: 'app-cntr',
-    templateUrl: './cntr.component.html',
-    providers: [CntrService]
+  selector: 'app-cntr',
+  templateUrl: './cntr.component.html',
+  providers: [CntrService]
 })
 export class CntrComponent {
-    // Local Variables 
-    title = 'Container List';
+  // Local Variables
+  title = 'Container List';
 
-    @Input() menuid: string = '';
-    @Input() type: string = '';
-    @Input() parentid: string = '';
-    @Input() RecordList: Containerm[] = [];
+  @Input() menuid: string = '';
+  @Input() type: string = '';
+  @Input() parentid: string = '';
+  @Input() RecordList: Containerm[] = [];
 
-    modal: any;
-    selectedId: string = '';
-    loading = false;
-    CntrTypes: string = "";
-    menu_record: any;
-
-    private errorMessage: string[] = [];
-
-    mode = 'ADD';
-    pkid = '';
-    ctr: number;
-    bDelete: boolean = false;
+  @Input() ht: string = "197px";
 
 
-    // Array For Displaying List
 
-    // Single Record for add/edit/view details
-    Record: Containerm = new Containerm;
+  modal: any;
+  selectedId: string = '';
+  loading = false;
+  CntrTypes: string = "";
+  menu_record: any;
 
-    constructor(
-        private ms: CntrService,
-        private route: ActivatedRoute,
-        private gs: GlobalService,
-        private modalService: NgbModal,
-    ) {
+  private errorMessage: string[] = [];
+
+  mode = 'ADD';
+  pkid = '';
+  ctr: number;
+  bDelete: boolean = false;
+
+
+  // Array For Displaying List
+
+  // Single Record for add/edit/view details
+  Record: Containerm = new Containerm;
+
+  constructor(
+    private ms: CntrService,
+    private route: ActivatedRoute,
+    private gs: GlobalService,
+    private modalService: NgbModal,
+  ) {
+  }
+
+  // Init Will be called After executing Constructor
+  ngOnInit() {
+    this.InitComponent();
+    // this.List('NEW');
+    this.ActionHandler("ADD", null);
+  }
+  InitComponent() {
+    this.bDelete = false;
+    this.menu_record = this.gs.getMenu(this.menuid);
+    if (this.menu_record) {
+      this.title = this.menu_record.menu_name;
+      if (this.menu_record.rights_delete)
+        this.bDelete = true;
+    }
+  }
+  // Destroy Will be called when this component is closed
+  ngOnDestroy() {
+
+  }
+
+  LoadDefault() {
+
+    // this.loading = true;
+    // let SearchData = {
+    //   pkid: this.parentid,
+    //   comp_code: this.gs.globalVariables.comp_code,
+    //   branch_code: this.gs.globalVariables.branch_code,
+
+    // };
+
+    // this.ErrorMessage = '';
+    // this.InfoMessage = '';
+    // this.ms.LoadDefault(SearchData)
+    //   .subscribe(response => {
+    //     this.loading = false;
+    //   },
+    //     error => {
+    //       this.loading = false;
+    //       this.ErrorMessage = this.gs.getError(error);
+    //       alert(this.ErrorMessage);
+    //     });
+
+  }
+
+
+  LovSelected(_Record: SearchTable) {
+
+    if (_Record.controlname == "CNTR-TYPE") {
+      this.Record.cntr_type_id = _Record.id;
+      this.Record.cntr_type_code = _Record.code;
     }
 
-    // Init Will be called After executing Constructor
-    ngOnInit() {
-        this.InitComponent();
-        // this.List('NEW');
+  }
+
+
+
+  //function for handling LIST/NEW/EDIT Buttons
+  ActionHandler(action: string, id: string) {
+    this.errorMessage = [];
+    if (action == 'LIST') {
+      this.mode = '';
+      this.pkid = '';
+    }
+    else if (action === 'ADD') {
+      this.mode = 'ADD';
+      this.ResetControls();
+      this.NewRecord();
+    }
+    else if (action === 'EDIT') {
+      this.mode = 'EDIT';
+      this.ResetControls();
+      this.pkid = id;
+      this.GetRecord(id);
+    }
+
+  }
+
+
+  ResetControls() {
+
+  }
+
+  List(_type: string) {
+    // this.loading = true;
+    // let SearchData = {
+    //     type: _type,
+    //     rowtype: this.type,
+    //     parentid: this.parentid,
+    //     company_code: this.gs.globalVariables.comp_code,
+    //     branch_code: this.gs.globalVariables.branch_code,
+    //     year_code: this.gs.globalVariables.year_code
+    // };
+    // this.errorMessage = [];
+    // this.ms.List(SearchData)
+    //     .subscribe(response => {
+    //         this.loading = false;
+    //         this.RecordList = response.list;
+    //         this.ActionHandler("ADD", null);
+    //     },
+    //         error => {
+    //             this.loading = false;
+    //             this.errorMessage.push(this.gs.getError(error));
+    //             this.gs.showToastScreen(this.errorMessage);
+    //         });
+  }
+
+  NewRecord() {
+
+    this.pkid = this.gs.getGuid();
+    this.Record = new Containerm();
+    this.Record.cntr_pkid = this.pkid;
+    this.Record.cntr_mbl_id = this.parentid;
+    this.Record.cntr_no = '';
+    this.Record.cntr_type_id = '';
+    this.Record.cntr_type_code = '';
+    this.Record.rec_mode = this.mode;
+    this.Record.rec_version = 0;
+  }
+
+  // Load a single Record for VIEW/EDIT
+  GetRecord(Id: string) {
+    this.loading = true;
+
+    let SearchData = {
+      pkid: Id,
+    };
+    this.errorMessage = [];
+
+    this.ms.GetRecord(SearchData)
+      .subscribe(response => {
+        this.loading = false;
+        this.LoadData(response.record);
+      },
+        error => {
+          this.loading = false;
+          this.errorMessage = this.gs.getErrorArray(this.gs.getError(error));
+          this.gs.showToastScreen(this.errorMessage);
+        });
+  }
+
+  LoadData(_Record: Containerm) {
+    this.Record = _Record;
+    this.Record.rec_mode = this.mode;
+  }
+  // Save Data
+  Save() {
+
+    if (!this.allvalid())
+      return;
+    this.loading = true;
+    this.errorMessage = [];
+
+    this.Record.rec_category = this.type;
+    this.Record._globalvariables = this.gs.globalVariables;
+    this.ms.Save(this.Record)
+      .subscribe(response => {
+        this.loading = false;
+        this.Record.rec_version = response.version;
+        // this.gs.showToastScreen(["Save Complete"]);
+        this.RefreshList();
         this.ActionHandler("ADD", null);
+      },
+        error => {
+          this.loading = false;
+          this.errorMessage = this.gs.getErrorArray(this.gs.getError(error));
+          this.gs.showToastScreen(this.errorMessage);
+        });
+  }
+
+  allvalid() {
+    let sError: string = "";
+    let bret: boolean = true;
+    this.errorMessage = [];
+
+    if (this.Record.cntr_no.length <= 0) {
+      bret = false;
+      this.errorMessage.push("Container No Cannot Be Blank");
     }
-    InitComponent() {
-        this.bDelete = false;
-        this.menu_record = this.gs.getMenu(this.menuid);
-        if (this.menu_record) {
-            this.title = this.menu_record.menu_name;
-            if (this.menu_record.rights_delete)
-                this.bDelete = true;
+    if (this.Record.cntr_type_id.length <= 0) {
+      bret = false;
+      this.errorMessage.push("Container Type Cannot Be Blank");
+    }
+
+    if (bret === false) {
+      this.gs.showToastScreen(this.errorMessage);
+    }
+    return bret;
+  }
+
+  RefreshList() {
+
+    if (this.RecordList == null)
+      return;
+    var REC = this.RecordList.find(rec => rec.cntr_pkid == this.Record.cntr_pkid);
+    if (REC == null) {
+      this.RecordList.push(this.Record);
+    }
+    else {
+      REC.cntr_no = this.Record.cntr_no;
+      REC.cntr_type_code = this.Record.cntr_type_code;
+      REC.cntr_seal_no = this.Record.cntr_seal_no;
+    }
+  }
+
+  selectRowId(id: string) {
+    this.selectedId = id;
+  }
+  getRowId() {
+    return this.selectedId;
+  }
+
+  DeleteRow(_rec: Containerm) {
+    this.errorMessage = [];
+    if (!confirm("Delete selected row")) {
+      return;
+    }
+
+    this.loading = true;
+    let SearchData = {
+      pkid: _rec.cntr_pkid,
+      mblid: _rec.cntr_mbl_id,
+      cntrno: _rec.cntr_no,
+      company_code: this.gs.globalVariables.comp_code,
+      branch_code: this.gs.globalVariables.branch_code,
+      user_code: this.gs.globalVariables.user_code
+    };
+    this.errorMessage = [];
+
+    this.ms.DeleteRecord(SearchData)
+      .subscribe(response => {
+        this.loading = false;
+        if (response.error) {
+          this.gs.showToastScreen([response.error]);
+        } else {
+          this.RecordList.splice(this.RecordList.findIndex(rec => rec.cntr_pkid == _rec.cntr_pkid), 1);
+          this.ActionHandler('ADD', null);
         }
+      },
+        error => {
+          this.loading = false;
+          this.errorMessage = this.gs.getErrorArray(this.gs.getError(error));
+          this.gs.showToastScreen(this.errorMessage);
+        });
+  }
+
+
+  Close() {
+    this.gs.ClosePage('home');
+  }
+
+  OnBlur(field: string) {
+    switch (field) {
+      case 'cntr_no': {
+        this.Record.cntr_no = this.Record.cntr_no.toUpperCase();
+        break;
+      }
     }
-    // Destroy Will be called when this component is closed
-    ngOnDestroy() {
-
-    }
-
-    LoadDefault() {
-
-        // this.loading = true;
-        // let SearchData = {
-        //   pkid: this.parentid,
-        //   comp_code: this.gs.globalVariables.comp_code,
-        //   branch_code: this.gs.globalVariables.branch_code,
-
-        // };
-
-        // this.ErrorMessage = '';
-        // this.InfoMessage = '';
-        // this.ms.LoadDefault(SearchData)
-        //   .subscribe(response => {
-        //     this.loading = false;
-        //   },
-        //     error => {
-        //       this.loading = false;
-        //       this.ErrorMessage = this.gs.getError(error);
-        //       alert(this.ErrorMessage);
-        //     });
-
-    }
-
-
-    LovSelected(_Record: SearchTable) {
-
-        if (_Record.controlname == "CNTR-TYPE") {
-            this.Record.cntr_type_id = _Record.id;
-            this.Record.cntr_type_code = _Record.code;
-        }
-
-    }
-
-
-
-    //function for handling LIST/NEW/EDIT Buttons
-    ActionHandler(action: string, id: string) {
-        this.errorMessage = [];
-        if (action == 'LIST') {
-            this.mode = '';
-            this.pkid = '';
-        }
-        else if (action === 'ADD') {
-            this.mode = 'ADD';
-            this.ResetControls();
-            this.NewRecord();
-        }
-        else if (action === 'EDIT') {
-            this.mode = 'EDIT';
-            this.ResetControls();
-            this.pkid = id;
-            this.GetRecord(id);
-        }
-
-    }
-
-
-    ResetControls() {
-
-    }
-
-    List(_type: string) {
-        // this.loading = true;
-        // let SearchData = {
-        //     type: _type,
-        //     rowtype: this.type,
-        //     parentid: this.parentid,
-        //     company_code: this.gs.globalVariables.comp_code,
-        //     branch_code: this.gs.globalVariables.branch_code,
-        //     year_code: this.gs.globalVariables.year_code
-        // };
-        // this.errorMessage = [];
-        // this.ms.List(SearchData)
-        //     .subscribe(response => {
-        //         this.loading = false;
-        //         this.RecordList = response.list;
-        //         this.ActionHandler("ADD", null);
-        //     },
-        //         error => {
-        //             this.loading = false;
-        //             this.errorMessage.push(this.gs.getError(error));
-        //             this.gs.showToastScreen(this.errorMessage);
-        //         });
-    }
-
-    NewRecord() {
-
-        this.pkid = this.gs.getGuid();
-        this.Record = new Containerm();
-        this.Record.cntr_pkid = this.pkid;
-        this.Record.cntr_mbl_id = this.parentid;
-        this.Record.cntr_no = '';
-        this.Record.cntr_type_id = '';
-        this.Record.cntr_type_code = '';
-        this.Record.rec_mode = this.mode;
-        this.Record.rec_version = 0;
-    }
-
-    // Load a single Record for VIEW/EDIT
-    GetRecord(Id: string) {
-        this.loading = true;
-
-        let SearchData = {
-            pkid: Id,
-        };
-        this.errorMessage = [];
-
-        this.ms.GetRecord(SearchData)
-            .subscribe(response => {
-                this.loading = false;
-                this.LoadData(response.record);
-            },
-                error => {
-                    this.loading = false;
-                    this.errorMessage = this.gs.getErrorArray(this.gs.getError(error));
-                    this.gs.showToastScreen(this.errorMessage);
-                });
-    }
-
-    LoadData(_Record: Containerm) {
-        this.Record = _Record;
-        this.Record.rec_mode = this.mode;
-    }
-    // Save Data
-    Save() {
-
-        if (!this.allvalid())
-            return;
-        this.loading = true;
-        this.errorMessage = [];
-
-        this.Record.rec_category = this.type;
-        this.Record._globalvariables = this.gs.globalVariables;
-        this.ms.Save(this.Record)
-            .subscribe(response => {
-                this.loading = false;
-                this.Record.rec_version = response.version;
-                // this.gs.showToastScreen(["Save Complete"]);
-                this.RefreshList();
-                this.ActionHandler("ADD", null);
-            },
-                error => {
-                    this.loading = false;
-                    this.errorMessage = this.gs.getErrorArray(this.gs.getError(error));
-                    this.gs.showToastScreen(this.errorMessage);
-                });
-    }
-
-    allvalid() {
-        let sError: string = "";
-        let bret: boolean = true;
-        this.errorMessage = [];
-
-        if (this.Record.cntr_no.length <= 0) {
-            bret = false;
-            this.errorMessage.push("Container No Cannot Be Blank");
-        }
-        if (this.Record.cntr_type_id.length <= 0) {
-            bret = false;
-            this.errorMessage.push("Container Type Cannot Be Blank");
-        }
-
-        if (bret === false) {
-            this.gs.showToastScreen(this.errorMessage);
-        }
-        return bret;
-    }
-
-    RefreshList() {
-
-        if (this.RecordList == null)
-            return;
-        var REC = this.RecordList.find(rec => rec.cntr_pkid == this.Record.cntr_pkid);
-        if (REC == null) {
-            this.RecordList.push(this.Record);
-        }
-        else {
-            REC.cntr_no = this.Record.cntr_no;
-            REC.cntr_type_code = this.Record.cntr_type_code;
-            REC.cntr_seal_no = this.Record.cntr_seal_no;
-        }
-    }
-
-    selectRowId(id: string) {
-        this.selectedId = id;
-    }
-    getRowId() {
-        return this.selectedId;
-    }
-
-    DeleteRow(_rec: Containerm) {
-        this.errorMessage = [];
-        if (!confirm("Delete selected row")) {
-            return;
-        }
-
-        this.loading = true;
-        let SearchData = {
-            pkid: _rec.cntr_pkid,
-            mblid: _rec.cntr_mbl_id,
-            cntrno: _rec.cntr_no,
-            company_code: this.gs.globalVariables.comp_code,
-            branch_code: this.gs.globalVariables.branch_code,
-            user_code: this.gs.globalVariables.user_code
-        };
-        this.errorMessage = [];
-
-        this.ms.DeleteRecord(SearchData)
-            .subscribe(response => {
-                this.loading = false;
-                if (response.error) {
-                    this.gs.showToastScreen([response.error]);
-                } else {
-                    this.RecordList.splice(this.RecordList.findIndex(rec => rec.cntr_pkid == _rec.cntr_pkid), 1);
-                    this.ActionHandler('ADD', null);
-                }
-            },
-                error => {
-                    this.loading = false;
-                    this.errorMessage = this.gs.getErrorArray(this.gs.getError(error));
-                    this.gs.showToastScreen(this.errorMessage);
-                });
-    }
-
-
-    Close() {
-        this.gs.ClosePage('home');
-    }
-
-    OnBlur(field: string) {
-        switch (field) {
-            case 'cntr_no': {
-                this.Record.cntr_no = this.Record.cntr_no.toUpperCase();
-                break;
-            }
-        }
-    }
+  }
 
 }
 
