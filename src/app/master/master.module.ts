fd65809db297be2ad7343df6158e9cd97d4949f9
Ajith <ajithkvs@gmail.com>
2024-10-23 14:09:24 +0530

    maaster unw

--- src/app/master/master.module.ts
+++ src/app/master/master.module.ts
@@ -6,25 +6,12 @@ import { MasterRoutingModule } from './master.routing.module';
 import { MasterComponent } from './master.component';
 import { ParamComponent } from './param/param.component';
 
-import { SysParamComponent } from './sysparam/sysparam.component';
-
 import { CustomerComponent } from './customer/customer.component';
-
-import { AddbookComponent } from './customer/addbook.component';
 
 import { AddressmComponent } from './customer/addressm.component';
 
-
-
-import { XmlComponent } from './xml/xml.component';
-
-import { AllnumComponent } from './allnum/allnum.component';
-
-import { CustdetComponent } from './customer/custdet.component';
-import { BankInfoComponent } from './bankinfo/bankinfo.component';
-
 import { AddbookdelComponent  } from './customer/addbookdel.component';
-import { BenfComponent   } from './customer/benf.component';
+
 import { CustGroupComponent } from './customer-group/cust-group.component';
 import { PoSettingComponent } from './po-setting/po-setting.component';
 
@@ -36,16 +23,9 @@ import { PoSettingComponent } from './po-setting/po-setting.component';
   declarations: [
     MasterComponent,
     ParamComponent,
-    SysParamComponent,
     CustomerComponent,
-    AddbookComponent,
     AddressmComponent,
-    XmlComponent,
-    AllnumComponent,
-    CustdetComponent,
-    BankInfoComponent,
     AddbookdelComponent,
-    BenfComponent,
     CustGroupComponent,
     PoSettingComponent
   ],
